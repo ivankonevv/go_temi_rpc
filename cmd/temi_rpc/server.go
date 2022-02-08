7c48b993a4297be41e6a5f3c3c082354940a1b41
ivankonevv <ivankonewvv@yandex.by>
2022-02-08 17:33:55 +0300

    cert generator

--- cmd/temi_rpc/server.go
+++ cmd/temi_rpc/server.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"crypto/tls"
+	"io/ioutil"
 	"net"
 	"os"
 	"temi_rpc/internal/roles"
 	"temi_rpc/pkg/api/v1"
-	handlers2 "temi_rpc/pkg/services/handlers"
+	handlers "temi_rpc/pkg/services/handlers"
 	"temi_rpc/platform/database"
 	"temi_rpc/tools"
 	"time"
@@ -13,6 +15,7 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
 )
 
@@ -22,30 +25,39 @@ func main() {
 	}
 	database.Connect()
 
+	backendCert, _ := ioutil.ReadFile(os.Getenv("SSL_CERT"))
+	backendKey, _ := ioutil.ReadFile(os.Getenv("SSL_KEY"))
+	cert, err := tls.X509KeyPair(backendCert, backendKey)
+	if err != nil {
+		logrus.Errorf("Cannot create SSL certificate")
+	}
+	creds := credentials.NewServerTLSFromCert(&cert)
+
 	jwtManager := tools.NewJWTManager(os.Getenv("JWT_SECRET_KEY"), time.Hour*10)
 
 	interceptor := tools.NewAuthInterceptor(jwtManager, roles.AccessibleRoles())
 	grpcServer := grpc.NewServer(
 		grpc.UnaryInterceptor(interceptor.Unary()),
 		grpc.StreamInterceptor(interceptor.Stream()),
+		grpc.Creds(creds),
 	)
 	listener, err := net.Listen("tcp", os.Getenv("SERVER_URL"))
 	if err != nil {
 		logrus.Errorf("Cannot create net listener: %v", err)
 	}
-	v1.RegisterMetalDoorsApiServer(grpcServer, &handlers2.DoorsApiServer{})
+	v1.RegisterMetalDoorsApiServer(grpcServer, &handlers.DoorsApiServer{})
 	logrus.Info("MetalDoors service connected...")
-	v1.RegisterWoodDoorsApiServer(grpcServer, &handlers2.WoodDoorsApiService{})
+	v1.RegisterWoodDoorsApiServer(grpcServer, &handlers.WoodDoorsApiService{})
 	logrus.Info("WoodDoors service connected...")
-	v1.RegisterColorsApiServer(grpcServer, &handlers2.WoodColorsApiService{})
+	v1.RegisterColorsApiServer(grpcServer, &handlers.WoodColorsApiService{})
 	logrus.Info("Colors service connected...")
-	v1.RegisterUserServiceServer(grpcServer, &handlers2.UserServer{})
-	authServer := handlers2.NewAuthServer(jwtManager)
+	v1.RegisterUserServiceServer(grpcServer, &handlers.UserServer{})
+	authServer := handlers.NewAuthServer(jwtManager)
 
 	v1.RegisterAuthServiceServer(grpcServer, authServer)
 
-	v1.RegisterInventoryServiceServer(grpcServer, &handlers2.InventoryServer{})
-	v1.RegisterCallbackServiceServer(grpcServer, &handlers2.CallbackServer{})
+	v1.RegisterInventoryServiceServer(grpcServer, &handlers.InventoryServer{})
+	v1.RegisterCallbackServiceServer(grpcServer, &handlers.CallbackServer{})
 
 	reflection.Register(grpcServer)
 	if err := grpcServer.Serve(listener); err != nil {
